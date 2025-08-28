e9ecd1651ab761d6b40ba8d0926a0d3d242878d5
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 18:18:53 -0500

    configuration

--- src/main/java/com/classes/MsvcClassesApplication.java
+++ src/main/java/com/classes/MsvcClassesApplication.java
@@ -2,9 +2,8 @@ package com.classes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MsvcClassesApplication {
 
     public static void main(String[] args) {
