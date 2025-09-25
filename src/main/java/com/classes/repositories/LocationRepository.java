ea109fb103cfbe474eebb17d79b8681749c37ecb
Raydberg Gabriel <raydbergg@gmail.com>
2025-09-25 09:29:05 -0500

    fix: update Dockerfile to correct file copy order and enhance dependency resolution fix: modify LocationRepository to use UUID as the identifier type

--- src/main/java/com/classes/repositories/LocationRepository.java
+++ src/main/java/com/classes/repositories/LocationRepository.java
@@ -2,9 +2,12 @@ package com.classes.repositories;
 
 
 import com.classes.entities.LocationEntity;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface  LocationRepository extends JpaRepository<LocationEntity, Long> {
+public interface  LocationRepository extends JpaRepository<LocationEntity, UUID> {
 }
